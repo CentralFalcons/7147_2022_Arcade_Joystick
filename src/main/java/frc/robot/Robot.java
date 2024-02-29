a5f1223192babfcbac372800d0bb303a13f44d53
Central Academy Students <centralfalconsrobotics@gee-edu.com>
2024-02-28 19:33:37 -0500

    arcade drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,16 +4,16 @@
 
 package frc.robot;
 
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import com.ctre.phoenix.motorcontrol.can.*;
-// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.util.sendable.SendableRegistry;
+// import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
@@ -21,19 +21,18 @@ import com.ctre.phoenix.motorcontrol.can.*;
  */
 public class Robot extends TimedRobot {
 
-  private final WPI_TalonSRX m_frontLeft = new WPI_TalonSRX(1);
-  private final WPI_TalonSRX m_rearLeft = new WPI_TalonSRX(2);
-  private final WPI_TalonSRX m_frontRight = new WPI_TalonSRX(3);
-  private final WPI_TalonSRX m_rearRight = new WPI_TalonSRX(4);
-
-  // private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
-  // private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_frontLeft::set, m_frontRight::set);
-  // private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_rearLeft::set, m_rearRight::set);
-  private final Joystick m_stick = new Joystick(0);
+  private WPI_TalonSRX m_frontLeft = new WPI_TalonSRX(1);
+  private WPI_TalonSRX m_rearLeft = new WPI_TalonSRX(2);
+  private WPI_TalonSRX m_frontRight = new WPI_TalonSRX(3);
+  private WPI_TalonSRX m_rearRight = new WPI_TalonSRX(4);
+  private DifferentialDrive m_robotDrive = new DifferentialDrive(m_frontLeft::set, m_frontRight::set);
+  private XboxController driver_controller = new XboxController(0);
 
   public Robot() {
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+    SendableRegistry.addChild(m_robotDrive, m_frontLeft);
+    SendableRegistry.addChild(m_robotDrive, m_rearLeft);
+    SendableRegistry.addChild(m_robotDrive, m_frontRight);
+    SendableRegistry.addChild(m_robotDrive, m_rearRight);
   }
 
   @Override
@@ -41,8 +40,10 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rearRight.set(TalonSRXControlMode.Follower,1);
-    m_rightMotor.setInverted(true);
+    m_rearLeft.set(TalonSRXControlMode.Follower,1);
+    m_rearRight.set(TalonSRXControlMode.Follower,3);
+    m_frontRight.setInverted(true);
+    m_rearRight.setInverted(true);
   }
 
   @Override
@@ -50,6 +51,6 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    m_robotDrive.arcadeDrive(-driver_controller.getRightY(), -driver_controller.getLeftX()*.5);
   }
 }
